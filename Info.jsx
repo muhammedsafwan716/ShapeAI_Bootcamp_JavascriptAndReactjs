02f8abceba5845307b2ba44b74aac31f64fa03e2
muhammedsafwan716 <61822350+muhammedsafwan716@users.noreply.github.com>
2021-06-27 22:26:24 +0530

    Create Info.jsx

---
+++ Info.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Info() {
+
+  return (
+
+  <div className="note">
+
+      <h1>Java Script and React Js</h1>
+
+      <p>A basic Webdevelopment React.js Bootcamp</p>
+
+  </div>
+
+   
+
+  );
+
+}
+
+export default Info;
